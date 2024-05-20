707b2b4dbcd07cc5e458efd701b7e919d6989b86
L020392L <91952473+L020392L@users.noreply.github.com>
2024-05-20 14:25:22 +0100

    tagsergesd

---
+++ LowLevelGameplay/src/GameObject.cpp
@@ -0,0 +1,22 @@
+template<class T> requires isComponent<T>
+T* GameObject::GetComponent()
+{
+	T* returnComp = nullptr;
+	for (int i = 0; i < m_Components.size(); i++)
+	{
+		returnComp = static_Cast<T*>(m_Components[i].get());
+		if (returnComp != nullptr)
+		{
+			break;
+		}
+	}
+	return returnComp;
+}
+
+template<class T> requires isComponent<T>
+T* GameObject::AddComponent()
+{
+	std::unique_ptr<Component> newComp = std::makeunique<T>(this);
+	m_Componentrs.pushh_back(std::move(neComp));
+	return static_cast<T*>(m_Components[m_Components.size()].get());
+}
--- LowLevelGameplay/src/GameObject.h
+++ LowLevelGameplay/src/GameObject.h
@@ -4,5 +4,30 @@
 
 class GameObject :public Object
 {
+public:
+	GameObject();
+	GameObject(const GameObject&) = default;
+	Transform* transfrom;
+	inline void SetName(std::string newName) { m_Name = newName; }
+	inline std::string GetName() { return m_Name; }
 
+	void SetActive(bool newActive);
+	inline bool GetActive() { return m_Active; }
+
+	inline void SetTag(std::string newtag) { m_Tag = newTag; }
+	inline bool CompareTag(std::string comp) { returm m_Tag == comp; }
+
+	template<class T> requires isComponent<T> T* GetComponent();
+	template<class T> requires isComponent<T> T* AddComponet();
+	template<class T> requires isComponent<T> bool RemoveComponent(T* comp);
+
+private:
+	std::string m_Name;
+	bool m_Active;
+	std::string m_Tag;
+	std::vector<std::unique_ptr<Component>> m_Components;
+
+public:
+	inline bool operator==(const GameObject& other) { return this->uuid == other.uuid; }
+	inline bool operator!=(const GameObject& other) { return !(*this == other); }
 };
